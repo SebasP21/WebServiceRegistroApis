6b44c7da9f643586bcbf563dd535a5df8749e613
Sebastian Peña <36645506+Sebascoman10@users.noreply.github.com>
2018-03-21 22:08:18 -0500

    Primera parte del taller
    Solo esta el consumo de webServices para los registros.

---
+++ WebServiceP/WebServiceP/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace WebServiceP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.apellido = new System.Windows.Forms.TextBox();
+            this.telefono = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(32, 193);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 0;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Escoja un pais";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Pais";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Formulario";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 274);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(151, 274);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(268, 274);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(387, 274);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(32, 346);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(206, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(32, 399);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 330);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Tarjeta";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(138, 399);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(148, 258);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Codigo pais";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(268, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Moneda";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(384, 258);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Codigo moneda";
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(110, 61);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(100, 20);
+            this.nombre.TabIndex = 17;
+            // 
+            // apellido
+            // 
+            this.apellido.Location = new System.Drawing.Point(110, 87);
+            this.apellido.Name = "apellido";
+            this.apellido.Size = new System.Drawing.Size(100, 20);
+            this.apellido.TabIndex = 18;
+            // 
+            // telefono
+            // 
+            this.telefono.Location = new System.Drawing.Point(110, 113);
+            this.telefono.Name = "telefono";
+            this.telefono.Size = new System.Drawing.Size(100, 20);
+            this.telefono.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(31, 61);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Nombre:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(31, 87);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Apellido:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(30, 113);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(52, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Telefono:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(33, 425);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Verificar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(244, 399);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(124, 20);
+            this.textBox8.TabIndex = 24;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 459);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(342, 42);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "Enviar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(110, 140);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(100, 20);
+            this.password.TabIndex = 26;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(32, 140);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(64, 13);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "Contraseña:";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(501, 513);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.telefono);
+            this.Controls.Add(this.apellido);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox3);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.TextBox apellido;
+        private System.Windows.Forms.TextBox telefono;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ WebServiceP/WebServiceP/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Drawing;
+
+using WebServiceP.DetailsCountry;
+using WebServiceP.ConfirmarEmail;
+using WebServiceP.ConfirmarTarjeta;
+
+using System.Data.SqlClient;
+namespace WebServiceP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            WebServiceP.DetailsCountry.country test = new WebServiceP.DetailsCountry.country();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WebServiceP.DetailsCountry.country test = new WebServiceP.DetailsCountry.country();
+            string ts = test.GetCountries();
+
+
+
+            //comboBox2.DataSource = Enum.GetValues(typeof());
+            //comboBox2.SelectedItem = ;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebServiceP.DetailsCountry.country test = new WebServiceP.DetailsCountry.country();
+            string ts = test.GetCountries();
+            ts = ts.Replace("<NewDataSet>", "");
+            ts = ts.Replace("<Table>", "");
+            ts = ts.Replace("</Table>", "");
+            ts = ts.Replace("</NewDataSet>", "");
+            ts = ts.Replace("<Name>", "");
+            ts = ts.Replace("</Name>", "~");
+            ts = ts.Replace("\n", "");
+            ts = ts.Replace("  ", "");
+
+            string[] tester = ts.Split('~');
+
+            for (int i = 0; i < tester.Length; i++)
+            {
+                comboBox3.Items.Add(tester[i]);
+                
+                
+
+                //comboBox3.DataSource = Enum.GetValues(typeof(tes));
+
+            }
+            
+            //string mon = test.GetCurrencies();
+            //mon = mon.Replace("<NewDataSet>", "");
+            //mon = mon.Replace("<Table>", "");
+            //mon = mon.Replace("</Table>", "");
+            //mon = mon.Replace("</NewDataSet>", "");
+            //mon = mon.Replace("<Name>", "~");
+            //mon = mon.Replace("</Name>", "");
+            //mon = mon.Replace("<CountryCode>", "");
+            //mon = mon.Replace("</CountryCode>", "§");
+            //mon = mon.Replace("<Currency>", "");
+            //mon = mon.Replace("</Currency>", "§");
+            //mon = mon.Replace("<CurrencyCode>", "");
+            //mon = mon.Replace("</CurrencyCode>", "§");
+            //mon = mon.Replace("<CurrencyCode />", "§");
+            //mon = mon.Replace("<Currency />", "§");
+            //mon = mon.Replace("\n", "");
+            //mon = mon.Replace(" ", "");
+
+            //string[] moneda = mon.Split('~');
+
+            //for (int i = 1; i < moneda.Length; i++)
+            //{
+            //    string[] valores = moneda[i].Split('§');
+
+            //    comboBox4.Items.Add(valores[0]);
+            //    comboBox5.Items.Add(valores[1]);
+            //    comboBox6.Items.Add(valores[2]);
+            //    comboBox7.Items.Add(valores[3]);
+                
+                
+
+            //}
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            WebServiceP.DetailsCountry.country test = new WebServiceP.DetailsCountry.country();
+            string m = test.GetCurrencyByCountry(Convert.ToString(comboBox3.SelectedItem));
+            m = m.Replace("<NewDataSet>", "");
+            m = m.Replace("<Table>", "§");
+            m = m.Replace("</Table>", "");
+            m = m.Replace("</NewDataSet>", "");
+            m = m.Replace("<Name>", "");
+            m = m.Replace("</Name>", "");
+            m = m.Replace("<CountryCode>", "§");
+            m = m.Replace("</CountryCode>", "");
+            m = m.Replace("<Currency>", "§");
+            m = m.Replace("</Currency>", "");
+            m = m.Replace("<CurrencyCode>", "§");
+            m = m.Replace("</CurrencyCode>", "");
+            m = m.Replace("<CurrencyCode />", "§");
+            m = m.Replace("<Currency />", "§");
+            m = m.Replace("\n", "");
+            m = m.Replace(" ", "");
+
+            string[] moneda = m.Split('§');
+            
+            textBox1.Text = moneda[1];
+            textBox2.Text = moneda[2];
+            textBox3.Text = moneda[3];
+            textBox4.Text = moneda[4];
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WebServiceP.ConfirmarEmail.ValidateEmail email = new WebServiceP.ConfirmarEmail.ValidateEmail();
+            
+            //Boolean em = email.IsValidEmail(Convert.ToString(textBox5.SelectedText));
+            if (email.IsValidEmail(textBox5.Text) == true)
+            {
+                textBox5.ForeColor = Color.Green;
+            }
+            else
+            {
+                textBox5.ForeColor = Color.Red;
+            }
+            WebServiceP.ConfirmarTarjeta.CCChecker tarjeta = new WebServiceP.ConfirmarTarjeta.CCChecker();
+
+            textBox8.Text = tarjeta.ValidateCardNumber(textBox6.Text, textBox7.Text);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("server=SEBASTIAN ; database=RegistroWebService ; integrated security = true");
+            conexion.Open();
+            MessageBox.Show("Se abrió la conexión con el servidor SQL Server y se seleccionó la base de datos");
+            //conexion.Close();
+            //MessageBox.Show("Se cerró la conexión.");
+       
+
+ 
+   SqlCommand myCommand = new SqlCommand("set_insertarRegistro ", conexion);
+
+   // Mark the Command as a SPROC
+   myCommand.CommandType = CommandType.StoredProcedure;
+
+   SqlParameter parameterNombre = new SqlParameter("@Nombre", SqlDbType.VarChar, 50);
+   parameterNombre.Value = nombre.Text;
+   myCommand.Parameters.Add(parameterNombre);
+   
+   SqlParameter parameterApellido= new SqlParameter("@Apellido", SqlDbType.VarChar, 50);
+   parameterApellido.Value = apellido.Text;
+   myCommand.Parameters.Add(parameterApellido);
+
+            SqlParameter parameterTelefono = new SqlParameter("@Telefono", SqlDbType.VarChar, 50);
+   parameterTelefono.Value = telefono.Text;
+   myCommand.Parameters.Add(parameterTelefono);
+            
+            SqlParameter parameterPais = new SqlParameter("@Pais", SqlDbType.VarChar, 50);
+   parameterPais.Value = textBox1.Text;
+   myCommand.Parameters.Add(parameterPais);
+            
+
+            SqlParameter parameterCodPais = new SqlParameter("@CodPais", SqlDbType.VarChar, 50);
+   parameterCodPais.Value = textBox2.Text;
+   myCommand.Parameters.Add(parameterCodPais);
+
+            SqlParameter parameterMoneda = new SqlParameter("@Moneda", SqlDbType.VarChar, 50);
+   parameterMoneda.Value = textBox3.Text;
+   myCommand.Parameters.Add(parameterMoneda);
+
+            SqlParameter parameterCodMoneda = new SqlParameter("@CodMoneda", SqlDbType.VarChar, 50);
+   parameterCodMoneda.Value = textBox4.Text;
+   myCommand.Parameters.Add(parameterCodMoneda);
+
+            SqlParameter parameterEmail = new SqlParameter("@Email", SqlDbType.VarChar, 50);
+   parameterEmail.Value = textBox5.Text;
+   myCommand.Parameters.Add(parameterEmail);
+
+            SqlParameter parameterTipoTarjeta = new SqlParameter("@TipoTarjeta", SqlDbType.VarChar, 50);
+   parameterTipoTarjeta.Value = textBox6.Text;
+   myCommand.Parameters.Add(parameterTipoTarjeta);
+
+            SqlParameter parameterNumTarjeta = new SqlParameter("@NumTarjeta", SqlDbType.VarChar, 50);
+   parameterNumTarjeta.Value = textBox7.Text;
+   myCommand.Parameters.Add(parameterNumTarjeta);
+
+            SqlParameter parameterPassword = new SqlParameter("@Password", SqlDbType.VarChar, 50);
+   parameterPassword.Value = password.Text;
+   myCommand.Parameters.Add(parameterPassword);
+
+            
+
+   SqlDataReader datDatos_t = myCommand.ExecuteReader();
+   //if (datDatos_t.Read()) 
+   //{
+   // this.strCodigo_l = strCodigo_p;
+   // this.strPais_l = datDatos_t.GetString(datDatos_t.GetOrdinal("CODPAI"));
+   // this.strNombre_l = datDatos_t.GetString(datDatos_t.GetOrdinal("NOMCIU"));
+   // this.intDepartamento_l = Int32.Parse((datDatos_t.GetValue(datDatos_t.GetOrdinal("CODDEP")).ToString()));
+   // this.strEstado_l =  datDatos_t.GetString(datDatos_t.GetOrdinal("ESTADO"));
+   //}
+   datDatos_t.Close();
+   conexion.Close();
+  }
+        }
+        
+    }
+
+
+
+
